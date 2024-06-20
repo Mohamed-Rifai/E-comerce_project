9dee06d6ebcc648d6e3c1731b9cacb620c4737ed
Mohamed <rifaeeckm@gmail.com>
2024-06-20 07:15:00 +0530

    db local changed into cluster

--- config/database-connetion.js
+++ config/database-connetion.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-mongoose.connect(process.env.MONG_URI).then(()=>{
+mongoose.connect(process.env.MONG_URI_CLUSTER).then(()=>{
     console.log('data base connected')
 })
 .catch((err)=>{
     
     console.log(err)
-})
+})  
